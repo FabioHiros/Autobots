4cd9e496623d0e90958f972390c0a8eaac030b11
fabio <fabio.matsumura@fatec.sp.gov.br>
2025-06-06 18:21:36 -0300

    small fixes

--- automanager/src/main/java/com/autobots/automanager/controles/DocumentoControle.java
+++ automanager/src/main/java/com/autobots/automanager/controles/DocumentoControle.java
@@ -18,9 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import com.autobots.automanager.entidades.Cliente;
 import com.autobots.automanager.entidades.Documento;
 import com.autobots.automanager.modelo.DocumentoAtualizador;
 import com.autobots.automanager.modelo.DocumentoSelecionador;
+import com.autobots.automanager.repositorios.ClienteRepositorio;
 import com.autobots.automanager.repositorios.DocumentoRepositorio;
 
 @RestController
@@ -30,6 +32,8 @@ public class DocumentoControle {
     private DocumentoRepositorio repositorio;
     @Autowired
     private DocumentoSelecionador selecionador;
+     @Autowired
+    private ClienteRepositorio clienteRepositorio;
 
     @GetMapping("/{id}")
     public ResponseEntity<EntityModel<Documento>> obterDocumento(@PathVariable long id) {
@@ -113,14 +117,28 @@ public class DocumentoControle {
         }
     }
 
-    @DeleteMapping("/{id}")
+ @DeleteMapping("/{id}")
     public ResponseEntity<Void> excluirDocumento(@PathVariable long id) {
-        if (repositorio.existsById(id)) {
-            Documento documento = repositorio.getById(id);
-            repositorio.delete(documento);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
+        if (!repositorio.existsById(id)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        
+        // Remove document from all clients first
+        List<Cliente> clientes = clienteRepositorio.findAll();
+        boolean wasRemoved = false;
+        for (Cliente cliente : clientes) {
+            boolean removed = cliente.getDocumentos().removeIf(doc -> doc.getId().equals(id));
+            if (removed) {
+                clienteRepositorio.save(cliente); // This triggers orphanRemoval and deletes the document
+                wasRemoved = true;
+            }
+        }
+        
+        // Only delete manually if it wasn't removed from any client
+        if (!wasRemoved && repositorio.existsById(id)) {
+            repositorio.deleteById(id);
+        }
+         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        
     }
 }
--- automanager/src/main/java/com/autobots/automanager/controles/EnderecoControle.java
+++ automanager/src/main/java/com/autobots/automanager/controles/EnderecoControle.java
@@ -1,8 +1,11 @@
 package com.autobots.automanager.controles;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,10 +16,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import com.autobots.automanager.entidades.Cliente;
 import com.autobots.automanager.entidades.Endereco;
 import com.autobots.automanager.modelo.EnderecoAtualizador;
 import com.autobots.automanager.modelo.EnderecoSelecionador;
+import com.autobots.automanager.repositorios.ClienteRepositorio;
 import com.autobots.automanager.repositorios.EnderecoRepositorio;
 
 @RestController
@@ -25,38 +31,75 @@ public class EnderecoControle {
     @Autowired
     private EnderecoRepositorio repositorio;
     @Autowired
+    private ClienteRepositorio clienteRepositorio;
+    @Autowired
     private EnderecoSelecionador selecionador;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Endereco> obterEndereco(@PathVariable long id) {
+    public ResponseEntity<EntityModel<Endereco>> obterEndereco(@PathVariable long id) {
         List<Endereco> enderecos = repositorio.findAll();
         Endereco endereco = selecionador.selecionar(enderecos, id);
         if (endereco == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(endereco, HttpStatus.OK);
+        
+        EntityModel<Endereco> enderecoModel = EntityModel.of(endereco);
+        enderecoModel.add(linkTo(methodOn(EnderecoControle.class).obterEndereco(id)).withSelfRel());
+        enderecoModel.add(linkTo(methodOn(EnderecoControle.class).atualizarEndereco(id, null)).withRel("update"));
+        enderecoModel.add(linkTo(methodOn(EnderecoControle.class).excluirEndereco(id)).withRel("delete"));
+        enderecoModel.add(linkTo(EnderecoControle.class).withRel("enderecos"));
+        
+        return new ResponseEntity<>(enderecoModel, HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<List<Endereco>> obterEnderecos() {
+    public ResponseEntity<CollectionModel<EntityModel<Endereco>>> obterEnderecos() {
         List<Endereco> enderecos = repositorio.findAll();
-        return new ResponseEntity<>(enderecos, HttpStatus.OK);
+        
+        List<EntityModel<Endereco>> enderecoModels = enderecos.stream()
+            .map(endereco -> {
+                EntityModel<Endereco> enderecoModel = EntityModel.of(endereco);
+                enderecoModel.add(linkTo(methodOn(EnderecoControle.class).obterEndereco(endereco.getId())).withSelfRel());
+                enderecoModel.add(linkTo(methodOn(EnderecoControle.class).atualizarEndereco(endereco.getId(), null)).withRel("update"));
+                enderecoModel.add(linkTo(methodOn(EnderecoControle.class).excluirEndereco(endereco.getId())).withRel("delete"));
+                return enderecoModel;
+            })
+            .collect(Collectors.toList());
+        
+        CollectionModel<EntityModel<Endereco>> collectionModel = CollectionModel.of(enderecoModels);
+        collectionModel.add(linkTo(EnderecoControle.class).withSelfRel());
+        collectionModel.add(linkTo(methodOn(EnderecoControle.class).cadastrarEndereco(null)).withRel("create"));
+        
+        return new ResponseEntity<>(collectionModel, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Endereco> cadastrarEndereco(@RequestBody Endereco endereco) {
-        repositorio.save(endereco);
-        return new ResponseEntity<>(endereco, HttpStatus.CREATED);
+    public ResponseEntity<EntityModel<Endereco>> cadastrarEndereco(@RequestBody Endereco endereco) {
+        Endereco enderecoSalvo = repositorio.save(endereco);
+        
+        EntityModel<Endereco> enderecoModel = EntityModel.of(enderecoSalvo);
+        enderecoModel.add(linkTo(methodOn(EnderecoControle.class).obterEndereco(enderecoSalvo.getId())).withSelfRel());
+        enderecoModel.add(linkTo(methodOn(EnderecoControle.class).atualizarEndereco(enderecoSalvo.getId(), null)).withRel("update"));
+        enderecoModel.add(linkTo(methodOn(EnderecoControle.class).excluirEndereco(enderecoSalvo.getId())).withRel("delete"));
+        enderecoModel.add(linkTo(EnderecoControle.class).withRel("enderecos"));
+        
+        return new ResponseEntity<>(enderecoModel, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> atualizarEndereco(@PathVariable long id, @RequestBody Endereco atualizacao) {
+    public ResponseEntity<EntityModel<Endereco>> atualizarEndereco(@PathVariable long id, @RequestBody Endereco atualizacao) {
         if (repositorio.existsById(id)) {
-            Endereco endereco = repositorio.getById(id);
+            Endereco endereco = repositorio.findById(id).get();
             EnderecoAtualizador atualizador = new EnderecoAtualizador();
             atualizador.atualizar(endereco, atualizacao);
-            repositorio.save(endereco);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            Endereco enderecoAtualizado = repositorio.save(endereco);
+            
+            EntityModel<Endereco> enderecoModel = EntityModel.of(enderecoAtualizado);
+            enderecoModel.add(linkTo(methodOn(EnderecoControle.class).obterEndereco(id)).withSelfRel());
+            enderecoModel.add(linkTo(methodOn(EnderecoControle.class).excluirEndereco(id)).withRel("delete"));
+            enderecoModel.add(linkTo(EnderecoControle.class).withRel("enderecos"));
+            
+            return new ResponseEntity<>(enderecoModel, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -64,12 +107,26 @@ public class EnderecoControle {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> excluirEndereco(@PathVariable long id) {
-        if (repositorio.existsById(id)) {
-            Endereco endereco = repositorio.getById(id);
-            repositorio.delete(endereco);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
+        if (!repositorio.existsById(id)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        
+        // Remove address from all clients first
+        List<Cliente> clientes = clienteRepositorio.findAll();
+        boolean wasRemoved = false;
+        for (Cliente cliente : clientes) {
+            if (cliente.getEndereco() != null && cliente.getEndereco().getId().equals(id)) {
+                cliente.setEndereco(null);
+                clienteRepositorio.save(cliente); // This triggers orphanRemoval and deletes the address
+                wasRemoved = true;
+            }
+        }
+        
+        // Only delete manually if it wasn't removed from any client
+        if (!wasRemoved && repositorio.existsById(id)) {
+            repositorio.deleteById(id);
+        }
+        
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
